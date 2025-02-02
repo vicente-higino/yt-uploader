db6f9630a9eb1d2287869b707db772e2667e5dd7
Vicente Higino <vi_higino@hotmail.com.br>
2025-02-02 00:50:10 -0300

    fix use correct title length with emojis

--- src/main.ts
+++ src/main.ts
@@ -65,21 +65,28 @@ async function deleteVOD(id: string) {
   }
 }
 
+function getStrLength(str: string): number {
+  return [...new Intl.Segmenter().segment(str)].length;
+}
+
+function strSlice(str: string, to: number): string {
+  return [...new Intl.Segmenter().segment(str)].filter((x, i) => i < to).map(x => x.segment).join("");
+}
+
 function truncateString(
   str: string,
   channelNameLength: number,
   maxLength: number = 100,
 ) {
-  if (str.length + (27 + channelNameLength) >= maxLength) {
-    return str.slice(0, maxLength - (27 + channelNameLength) - 3) + "...";
+  if (getStrLength(str) + (27 + channelNameLength) >= maxLength) {
+    return strSlice(str, maxLength - (27 + channelNameLength) - 3) + "...";
   }
   return str;
 }
 
 function makeTitle(date: string, data: { title: string; user_name: string }) {
-  return `[${date}] ${
-    truncateString(data.title, data.user_name.length)
-  } [${data.user_name.toUpperCase()} TWITCH VOD]`;
+  return `[${date}] ${truncateString(data.title, data.user_name.length)
+    } [${data.user_name.toUpperCase()} TWITCH VOD]`;
 }
 
 async function makeThumbnail(text: string, path: string, outputPath: string) {
